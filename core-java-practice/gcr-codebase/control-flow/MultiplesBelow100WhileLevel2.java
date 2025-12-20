eb81ff39cd00663cbd35e514574979b74f17be90
Aakash Attri <aakashattri516@gmail.com>
2025-12-20 11:06:42 +0530

    [Aakash] Added : Solution of multiples below 100 using while loo[ lvl 2 in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/MultiplesBelow100WhileLevel2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner ;
+public class MultiplesBelow100WhileLevel2 {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        int counter = 1;
+        while (counter < 100) {
+
+            if (counter % number == 0)
+
+                System.out.println(counter);
+
+            counter++;
+        }
+    }
+}
