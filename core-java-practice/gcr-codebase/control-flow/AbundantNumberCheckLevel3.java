40ab72893a4d4571d9ca4286418ee62476500baf
Aakash Attri <aakashattri516@gmail.com>
2025-12-20 11:09:21 +0530

    [Aakash] Added : Solution of Abdundant number check lvl 3 in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/AbundantNumberCheckLevel3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AbundantNumberCheckLevel3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum > num) {
+            System.out.println(num + " Abundant Number");
+        } else {
+            System.out.println(num + " Abundant Number");
+        }
+    }
+}
