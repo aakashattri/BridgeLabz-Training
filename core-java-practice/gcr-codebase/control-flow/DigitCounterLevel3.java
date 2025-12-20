fc13bb7dbc8af679cb5971f922af1c8ce44bafb1
Aakash Attri <aakashattri516@gmail.com>
2025-12-20 11:12:18 +0530

    [Aakash] Added : Solution of digital counter lvl 3 in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/DigitCounterLevel3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DigitCounterLevel3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        while (num != 0) {
+            num /= 10;
+
+            count++;
+
+        }
+
+        System.out.println("Total digits: " + count);
+    }
+}
