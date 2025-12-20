cbbc8a653e7ecdd4196e0f9ee2a81af994eed284
Aakash Attri <aakashattri516@gmail.com>
2025-12-20 21:18:07 +0530

    [Aakash] Added: Completed Solution of Mean of height Lvl 1 in gcr-codebase/arrays

---
+++ core-java-practice/gcr-codebase/arrays/HeightMean.java
@@ -0,0 +1,21 @@
+import java.util.* ;
+public class HeightMean {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        double[]  height = new double[11] ;
+
+        double sum = 0 ;
+
+        for(int i = 0;  i < 11; i++){
+
+            height[i] = sc.nextDouble()  ;
+            sum += height[i] ;
+
+        }
+
+        double  mean  =  sum / 11.0f ;
+        System.out.println("Mean Height : "  + mean) ;
+
+    }
+}
