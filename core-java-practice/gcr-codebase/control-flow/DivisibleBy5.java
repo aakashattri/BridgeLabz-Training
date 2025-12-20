b715969b419629f945c971e3af67b47204a7b159
Aakash Attri <aakashattri516@gmail.com>
2025-12-20 10:53:31 +0530

    [Aakash] Added : Solution of Number Divisble By 5 in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/DivisibleBy5.java
@@ -0,0 +1,9 @@
+public class DivisibleBy5 {
+    static void checkDivisible(int number) {
+
+        System.out.println(number % 5 == 0);
+    }
+    public static void main(String[] args) {
+        checkDivisible(25);
+    }
+}
