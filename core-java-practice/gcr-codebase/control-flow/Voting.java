4d830476238dee8910eb825cd2e8138316cbcda1
Aakash Attri <aakashattri516@gmail.com>
2025-12-20 10:55:20 +0530

    [Aakash] Added : Solution of Voting in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/Voting.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Voting {
+    static void check(int age) {
+        if (age >= 18) {
+            System.out.println("can vote.");
+        } else {
+            System.out.println("cannot vote.");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in ) ;
+        int age  =  sc.nextInt() ;
+
+        check(age);
+    }
+}
