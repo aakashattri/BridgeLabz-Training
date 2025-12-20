8c458e31118b9174c47a5892c8cc6e3180c0c647
Aakash Attri <aakashattri516@gmail.com>
2025-12-20 10:56:23 +0530

    [Aakash] Added : Solution of SumOfNaturalNumbers in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/SumOfNaturalNumbers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class SumOfNaturalNumbers{
+    static void compareSum(int n) {
+        if (n >= 1) {
+            int sum = 0, i = 1;
+            while (i <= n) {
+                sum += i;
+                i++;
+            }
+            int formulaSum = n * (n + 1) / 2;
+            System.out.println("Sum (loop): " + sum + ", Sum (formula): " + formulaSum);
+        } else {
+            System.out.println("Not a natural number");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt() ;
+        compareSum(n);
+    }
+}
