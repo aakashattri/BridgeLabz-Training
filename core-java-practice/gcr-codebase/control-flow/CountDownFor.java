526aadc26fbc560d8e90b9234f5bcc7c7f8fa81a
Aakash Attri <aakashattri516@gmail.com>
2025-12-20 10:58:07 +0530

    [Aakash] Added : Solution of Count Down  in gcr-codebase/control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/CountDownFor.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class CountDownFor {
+    static void count(int num) {
+        for (int i = num; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        int number = sc.nextInt() ;
+        count(number);
+    }
+}
