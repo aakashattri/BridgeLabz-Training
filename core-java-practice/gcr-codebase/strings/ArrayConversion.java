1145c33f61310ec2662a8438f0176e8b19e686f4
Aakash Attri <aakashattri516@gmail.com>
2025-12-22 14:39:39 +0530

    [Aakash]  Added: Solution of Array Conversion in gcr-codebase/strings

---
+++ core-java-practice/gcr-codebase/strings/ArrayConversion.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ArrayConversion {
+    public static  char[]  charManual (String text) {
+        char[]  chars = new   char[text.length()];
+        for (int i = 0; i <  text.length(); i++) {
+            chars [i] = text.charAt (i);
+        }
+        return  chars;
+    }
+
+    public static  boolean compareCharArrays(char[] a, char[] b) {
+        if (a.length != b.length) return false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] !=  b[i])
+                return false;
+        }
+        return true;
+    }
+
+    public static void  main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc. next();
+
+        char[] manual =  charManual(text);
+        char[] builtin =  text.toCharArray();
+
+        System.out.println( compareCharArrays(manual, builtin));
+    }
+}
