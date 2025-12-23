2d37e4e57f034124de0eea070a0782266790a4c4
Aakash Attri <aakashattri516@gmail.com>
2025-12-23 11:24:04 +0530

    [Aakash] Added: Solution of Remove Duplicates in gcr-codebase/java-string-extras

---
+++ core-java-practice/gcr-codebase/java-string-extras/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+    public static String rmvDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        for ( int i = 0;i < s.length() ; i++) {
+            char c = s.charAt(i);
+
+            if (    sb.indexOf(String.valueOf(c)) == -1)
+                sb.append(c);
+        }
+
+        return  sb.toString();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.println("Without duplicates: " + rmvDuplicates(s));
+    }
+}
