6a0a6a1fecaf1d4aabd3b521881f37d6685850f9
Aakash Attri <aakashattri516@gmail.com>
2025-12-23 10:36:26 +0530

    [Aakash]  Added: Solution of Handshake in gcr-codebase/methods

---
+++ core-java-practice/gcr-codebase/methods/Handshakes.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Handshakes {
+    public static int handshakes(int students) {
+        return (
+                students * (students - 1)
+
+        ) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        System.out.println("Maximum number of handshakes: " + handshakes(n));
+    }
+}
