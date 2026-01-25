25c96ce34eb99c879e156c4eb94f442472c9e05f
Aakash Attri <aakashattri516@gmail.com>
2026-01-25 18:50:31 +0530

    [Aakash] Added : Completed the solutions of json

---
+++ gcr-codebase/json/CreatingJsonObject.java
@@ -0,0 +1,12 @@
+
+public class CreatingJsonObject {
+	public static void main(String[] args) {
+		JSONObject jsonObject=new JSONObject();
+		jsonObject.put("name","Anurag");
+		jsonObject.put("email","anurag@gmail.com");
+		jsonObject.put("Phone","+918945567856");
+		System.out.println(jsonObject.toString());
+		
+		
+		}
+}
